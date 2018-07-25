20d9227523798ebe5229d74e3b30d57182003cd9
Carlos CANA <carloscana@gmail.com>
2018-07-25 22:03:57 +0200

    L'equipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
 10.times do
-  cour = Cour.create(title: rand(1..10), description: Faker::Educator.course)
+  cour = Cour.create(title: Faker::Educator.course, description: Faker::Friends.quote)
 end
 
 20.times do
